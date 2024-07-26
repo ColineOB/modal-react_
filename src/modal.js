1663caa678d14a31f6d9ee522907ac33e548eba3
ColineC <coline@hesiode.io>
2024-07-26 13:45:12 +0200

    style

--- src/modal.js
+++ src/modal.js
@@ -5,19 +5,11 @@ const ModalComponent = ({
   open = false,
   onClose = () => {},
   closeText = 'x',
-  spinnerElement = (
-    <div>
-      <div className='rect1' />
-      <div className='rect2' />
-      <div className='rect3' />
-      <div className='rect4' />
-    </div>
-  ),
+  spinnerElement = <div className={styles.spinner}></div>,
   showSpinner = true,
   fadeDuration = '250',
   fadeDelay = '0',
   opacityBackground = '0.7',
-  textAlign = 'left',
   children
 }) => {
   const [display, setDisplay] = useState('none')
@@ -41,14 +33,17 @@ const ModalComponent = ({
   }
 
   return (
-    <div className={`${styles.modal} ${animationClass}`} style={styleG}>
-      <div className={styles.modalBox}>
-        <div className={styles.modalBoxClose} onClick={onClose}>
-          {closeText}
+    <>
+      {spinnerElement}
+      <div className={`${styles.modal} ${animationClass}`} style={styleG}>
+        <div className={styles.modalBox}>
+          <div className={styles.modalBoxClose} onClick={onClose}>
+            {closeText}
+          </div>
+          {children}
         </div>
-        {children}
       </div>
-    </div>
+    </>
   )
 }
 
